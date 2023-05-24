1e8fbefd5715897abacf8ea6b39c4c4bf83a9f9b
srayo16 <srayosikder@gmail.com>
2023-05-24 13:17:41 +0600

    basic

---
+++ dynamicData.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int *a = new int;
+    *a = 10;
+    cout << *a;
+
+    // int x = 100;
+    // int *a = &x;
+    // *a = 10;
+    // cout << *a;
+
+    return 0;
+}
